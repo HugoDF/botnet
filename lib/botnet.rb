0acb7710e58416b948458bd04d108a81778ab087
HugoDF <hdifrancesco@gmail.com>
2016-05-07 14:05:34 +0100

    Fix typos

--- lib/botnet.rb
+++ lib/botnet.rb
@@ -3,20 +3,21 @@ get '/' do
   erb :index
 end
 post '/dns' do
-  repond_message "DNS Lookup"
+  # A, CNAME, AAAA, MX, NS
+  respond_message "DNS Lookup"
 end
 post '/domain' do
   # is domain taken or not, suggest to use whois if not
-  repond_message "domain"
+  respond_message "domain"
 end
 post '/whois' do
-  repond_message "whois"
+  respond_message "whois"
 end
 post '/ping' do 
-  repond_message "ping"
+  respond_message "ping"
 end
 post '/net' do
-  repond_message "Help & feedback"
+  respond_message "Help & feedback"
 end
 def respond_message message
   content_type :json
