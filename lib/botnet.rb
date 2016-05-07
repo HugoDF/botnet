cef62656cc65ec3215baf5d98a16a5b128303bf6
Wilhelm Klopp <wil.klopp@gmail.com>
2016-05-07 13:23:46 +0100

    Add trailing slashes :smile:

--- lib/botnet.rb
+++ lib/botnet.rb
@@ -2,20 +2,20 @@ require 'json'
 get '/' do
   erb :index
 end
-post '/dns' do
+post '/dns/' do
   repond_message "DNS Lookup"
 end
-post '/domain' do
+post '/domain/' do
   # is domain taken or not, suggest to use whois if not
   repond_message "domain"
 end
-post '/whois' do
+post '/whois/' do
   repond_message "whois"
 end
-post '/ping' do 
+post '/ping/' do 
   repond_message "ping"
 end
-post '/net' do
+post '/net/' do
   repond_message "Help & feedback"
 end
 def respond_message message
