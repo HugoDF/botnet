c31fac6a60ccb2fcb8ca5b7e2d611f5688f177aa
Wilhelm Klopp <wil.klopp@gmail.com>
2016-05-07 16:42:43 +0100

    Add whois and domain code :watch:

--- lib/botnet.rb
+++ lib/botnet.rb
@@ -1,8 +1,11 @@
 require 'json'
+require 'whois'
+
+
 get '/' do
   erb :index
 end
-post '/dns' do
+post '/dns/' do
   # A, CNAME, AAAA, MX, NS
   respond_message "DNS Lookup"
 end
@@ -11,14 +14,32 @@ post '/domain/' do
   respond_message "domain"
 end
 
-post '/whois' do
+post '/whois/' do
   respond_message "whois"
 end
-post '/ping' do 
+post '/ping/' do 
   respond_message "ping"
 end
-post '/net' do
+post '/net/' do
   respond_message "Help & feedback"
+end
+# for now only
+get '/whois/?' do
+    result = Whois.whois("simplepoll.rocks")
+    puts result
+    result.to_json
+end
+
+get '/ping/?' do # can't work on this, because windows :(
+    check = Net::Ping::External.new(host)
+    puts check
+    "Done"
+end
+
+get '/domain/:domain/?' do
+    result = Whois.whois(params[:domain])
+    is_available = result.available? ? "yes" : "no"
+    "Is " + params[:domain] + " available? " + is_available.to_s # this is some really messed up shit
 end
 def respond_message message
   content_type :json
